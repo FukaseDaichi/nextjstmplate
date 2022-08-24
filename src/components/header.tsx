d601abf91d9d0fdfdbb0ba09c90709d2ba8a0f3f
ITSVAPP <it.da.fukase@iftc.co.jp>
2022-08-24 16:42:16 +0900

    yarnからnpm対応

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,12 +1,11 @@
 import Link from "next/link";
-import type { VFC } from "react";
 
 const items = [
   { href: "/", label: "Home" },
   { href: "/about", label: "About" },
 ];
 
-export const Header: VFC = () => {
+export const Header: React.FC = () => {
   return (
     <header>
       <h1>Title</h1>
